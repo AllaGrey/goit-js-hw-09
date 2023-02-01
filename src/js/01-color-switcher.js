7ef2aa632471f8a87ee717d310484ffc549756d2
AllaGrey <alla.basanko2012@gmail.com>
2023-02-02 01:10:44 +0200

    add Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const changeBodyStyle = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+let isActive = false;
 
+btnStart.addEventListener('click', onclickStart);
+btnStop.addEventListener('click', onclickStop);
+
+function onclickStart() {
+  start();
+}
+
+function onclickStop() {
+  clearInterval(timerId);
+  isActive = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function start() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  timerId = setInterval(() => {
+    changeBodyStyle.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
