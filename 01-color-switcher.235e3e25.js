d9090f132a736b151ac44f2aa4467313491a9d22
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-02-06 20:43:03 +0000

    Deploying to gh-pages from @ AllaGrey/goit-js-hw-09@47b03c2890aa4890febf62c976243d0160846cc1 🚀

--- 01-color-switcher.235e3e25.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),n=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=null,r=!1;n.addEventListener("click",(function(){!function(){if(r)return;r=!0,o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}()})),e.addEventListener("click",(function(){clearInterval(o),r=!1}))}();
-//# sourceMappingURL=01-color-switcher.235e3e25.js.map
