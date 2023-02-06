826ef0acbea1845ee2c54b665ad635e7cd5b8376
AllaGrey <alla.basanko2012@gmail.com>
2023-02-06 10:42:59 +0200

    add Task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,61 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const inputEl = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    console.log(selectedDate);
+
+    if (selectedDate <= Date.now()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      startBtn.addEventListener('click', onClickStartBtn);
+    }
+
+    function onClickStartBtn() {
+      startBtn.disabled = true;
+      inputEl.disabled = true;
+
+      if (startBtn.disabled) {
+        setInterval(countTimeToTargetDate, 1000);
+      }
+    }
+
+    function countTimeToTargetDate() {
+      const currentDate = Date.now();
+      const diff = selectedDate - currentDate;
+
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+
+      const spanDays = document.querySelector('.value[data-days]');
+      const spanHours = document.querySelector('.value[data-hours]');
+      const spanMinutes = document.querySelector('.value[data-minutes]');
+      const spanSeconds = document.querySelector('.value[data-seconds]');
+
+      spanDays.textContent = addLeadingZero(days);
+      spanHours.textContent = addLeadingZero(hours);
+      spanMinutes.textContent = addLeadingZero(minutes);
+      spanSeconds.textContent = addLeadingZero(seconds);
+    }
+
+    function addLeadingZero(number) {
+      return String(number).padStart(2, 0);
+    }
+  },
+};
+
+flatpickr(inputEl, options);
