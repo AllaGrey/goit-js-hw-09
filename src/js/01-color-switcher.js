47b03c2890aa4890febf62c976243d0160846cc1
AllaGrey <alla.basanko2012@gmail.com>
2023-02-06 22:42:36 +0200

    update Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,31 +2,26 @@ const changeBodyStyle = document.querySelector('body');
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 let timerId = null;
-let isActive = false;
+btnStop.disabled = true;
 
 btnStart.addEventListener('click', onclickStart);
 btnStop.addEventListener('click', onclickStop);
 
 function onclickStart() {
-  start();
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  timerId = setInterval(() => {
+    changeBodyStyle.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 }
 
 function onclickStop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
   clearInterval(timerId);
-  isActive = false;
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function start() {
-  if (isActive) {
-    return;
-  }
-
-  isActive = true;
-  timerId = setInterval(() => {
-    changeBodyStyle.style.backgroundColor = getRandomHexColor();
-  }, 1000);
 }
