6a473c892e17a0df99a0f05be03ec046b625b883
AllaGrey <alla.basanko2012@gmail.com>
2023-02-07 12:07:57 +0200

    update Task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,6 +8,7 @@ const startBtn = document.querySelector('button[data-start]');
 startBtn.disabled = true;
 
 let timerId = null;
+let selectedDate;
 
 const options = {
   enableTime: true,
@@ -15,8 +16,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    console.log(selectedDate);
+    selectedDate = selectedDates[0];
 
     if (selectedDate <= Date.now()) {
       Notiflix.Notify.failure('Please choose a date in the future');
@@ -24,44 +24,44 @@ const options = {
       startBtn.disabled = false;
       startBtn.addEventListener('click', onClickStartBtn);
     }
-
-    function onClickStartBtn() {
-      startBtn.disabled = true;
-      inputEl.disabled = true;
-
-      if (startBtn.disabled) {
-        timerId = setInterval(countTimeToTargetDate, 1000);
-      }
-    }
-
-    function countTimeToTargetDate() {
-      const currentDate = Date.now();
-      const diff = selectedDate - currentDate;
-
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-      const minutes = Math.floor((diff / (1000 * 60)) % 60);
-      const seconds = Math.floor((diff / 1000) % 60);
-
-      const spanDays = document.querySelector('.value[data-days]');
-      const spanHours = document.querySelector('.value[data-hours]');
-      const spanMinutes = document.querySelector('.value[data-minutes]');
-      const spanSeconds = document.querySelector('.value[data-seconds]');
-
-      if (diff > 0) {
-        spanDays.textContent = addLeadingZero(days);
-        spanHours.textContent = addLeadingZero(hours);
-        spanMinutes.textContent = addLeadingZero(minutes);
-        spanSeconds.textContent = addLeadingZero(seconds);
-      } else {
-        clearInterval(timerId);
-      }
-    }
-
-    function addLeadingZero(number) {
-      return String(number).padStart(2, 0);
-    }
   },
 };
 
 flatpickr(inputEl, options);
+
+function onClickStartBtn() {
+  startBtn.disabled = true;
+  inputEl.disabled = true;
+
+  if (startBtn.disabled) {
+    timerId = setInterval(countTimeToTargetDate, 1000);
+  }
+}
+
+function countTimeToTargetDate() {
+  const currentDate = Date.now();
+  const diff = selectedDate - currentDate;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  const spanDays = document.querySelector('.value[data-days]');
+  const spanHours = document.querySelector('.value[data-hours]');
+  const spanMinutes = document.querySelector('.value[data-minutes]');
+  const spanSeconds = document.querySelector('.value[data-seconds]');
+
+  if (diff > 0) {
+    spanDays.textContent = addLeadingZero(days);
+    spanHours.textContent = addLeadingZero(hours);
+    spanMinutes.textContent = addLeadingZero(minutes);
+    spanSeconds.textContent = addLeadingZero(seconds);
+  } else {
+    clearInterval(timerId);
+  }
+}
+
+function addLeadingZero(number) {
+  return String(number).padStart(2, 0);
+}
